ef0a653be50433749e32398bda8d03ddbf3cc699
Travis Cross <tc@traviscross.com>
2014-07-22 02:06:29 +0000

    Refactor

--- test.c
+++ test.c
@@ -17,7 +17,6 @@ int main(int argc, char **argv) {
   } else if (len > 0 && res) {
     printf("Matched %d chars of key but no leaf value\n", len);
   }
-  int n = bit_trie_free(node);
-  printf("Freed %d nodes\n", n);
+  printf("Freed %d nodes\n", bit_trie_free(node));
   return 0;
 }
